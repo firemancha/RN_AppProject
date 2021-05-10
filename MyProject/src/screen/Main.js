5cc4d081fb6db4832b1204cae333ece0837fcb5c
Cha_Y_S <vaite714@gmail.com>
2021-05-11 00:34:55 +0900

    수정: Main.js / 배경 그라데이션 설정

--- MyProject/src/screen/Main.js
+++ MyProject/src/screen/Main.js
@@ -4,13 +4,25 @@ import {
     View,
     Text,
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Container from '../component/Container';
 
 class MainScreen extends Component {
     render(){
         return(
-            <View style = {styles.container}>
-                <Text>This is Main Screen</Text>
-            </View>
+            // <View style = {styles.container}>
+            //     <Text>This is Main Screen</Text>
+            // </View>
+            <LinearGradient
+                locations={[0.01, 0.2, 0.7, 1]}
+                colors={['#ffce83', '#97e274',  '#4ae4f2', '#d392fc']}
+                useAngle={true}
+                angle={45}
+                angleCenter={{x: 0.5, y: 0.5}}
+                style={styles.container}
+            >
+                <Text>Main Screen!</Text>
+            </LinearGradient>
         );
     };
 };
@@ -20,6 +32,7 @@ export default MainScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#ff00ff',
+        justifyContent: 'center',
+        alignItems: 'center',
 	}
 })
