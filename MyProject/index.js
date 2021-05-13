0122d776d102b14912c0113c68573f3b2fd5571a
Cha_Y_S <vaite714@gmail.com>
2021-05-14 03:55:57 +0900

    추가: index.js / 윈도우 size 전역 변수 추가

--- MyProject/index.js
+++ MyProject/index.js
@@ -3,8 +3,16 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Dimensions} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const STATUS_COLOR = "#fff5d8";
+
+global.WIDTH = WINDOW_WIDTH;
+global.HEIGHT = WINDOW_HEIGHT;
+global.STATUSCOLOR = STATUS_COLOR;
 
 AppRegistry.registerComponent(appName, () => App);
