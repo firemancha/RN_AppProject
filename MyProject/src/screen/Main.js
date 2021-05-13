9a7ecc8fa26acebc42c3e9a8ea059a6933901300
Cha_Y_S <vaite714@gmail.com>
2021-05-14 03:58:09 +0900

    수정: Main.js / MainScreen 일시적 수정

--- MyProject/src/screen/Main.js
+++ MyProject/src/screen/Main.js
@@ -5,17 +5,16 @@ import {
     Text,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import Container from '../component/Container';
+// import Container from '../component/DefaultStyle';
 
 class MainScreen extends Component {
+    
     render(){
+        // console.log("MainScreen");
         return(
-            // <View style = {styles.container}>
-            //     <Text>This is Main Screen</Text>
-            // </View>
             <LinearGradient
                 locations={[0.01, 0.2, 0.7, 1]}
-                colors={['#ffce83', '#97e274',  '#4ae4f2', '#d392fc']}
+                colors={['#fffbea', '#fff6d0',  '#fff1b6', '#d392fc']}
                 useAngle={true}
                 angle={45}
                 angleCenter={{x: 0.5, y: 0.5}}
@@ -32,7 +31,13 @@ export default MainScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+        // width: '100%',
+        // height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
+        // backgroundColor: '#ff0000',
+        // position: 'absolute',
+        // zIndex: 101,
+        
 	}
 })
