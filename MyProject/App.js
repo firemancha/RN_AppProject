37d71e9e4a97d16e8f88e01eddec89251bd153ce
Cha_Y_S <vaite714@gmail.com>
2021-05-14 03:55:22 +0900

    수정: App.js / 상태바 색상 변경

--- MyProject/App.js
+++ MyProject/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './src/screen/Main';
+import SignScreen from './src/screen/Sign';
+
 
 const Stack = createStackNavigator();
 
 class App extends Component{
+
+	componentDidMount(){
+		StatusBar.setBackgroundColor("transparent");
+  	StatusBar.setTranslucent(true);
+  	StatusBar.setBarStyle("dark-content");
+	}
 	render() {
 		return (
 			<NavigationContainer>
 				<Stack.Navigator 
 					screenOptions={{ headerShown: false }}
-					initialRouteName="Main"
+					initialRouteName="Sign"
 				>
 					<Stack.Screen name="Main" component={MainScreen} />
+					<Stack.Screen name="Sign" component={SignScreen} />
 				</Stack.Navigator>
 			</NavigationContainer>
-			
 		);
 	};
 };
