b0839a034a5b17ff11a10e0bd027ef077ca62cf6
Cha_Y_S <vaite714@gmail.com>
2021-05-14 03:57:27 +0900

    추가: index.js / 윈도우 size 전역 변수 추가

--- MyProject/index.js
+++ MyProject/index.js
@@ -9,10 +9,8 @@ import {name as appName} from './app.json';
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 const WINDOW_HEIGHT = Dimensions.get('window').height;
-const STATUS_COLOR = "#fff5d8";
 
 global.WIDTH = WINDOW_WIDTH;
 global.HEIGHT = WINDOW_HEIGHT;
-global.STATUSCOLOR = STATUS_COLOR;
 
 AppRegistry.registerComponent(appName, () => App);
