10bf2eb9ac611fea43c469dc846d027139488c58
Cha_Y_S <vaite714@gmail.com>
2021-05-14 03:59:24 +0900

    추가: Join.js / 회원가입 버튼 추가

---
+++ MyProject/src/component/Join.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+class Join extends Component{
+  render(){
+    return(
+      <TouchableOpacity style={styles.button}>
+        <View style={styles.icon_container}>
+          <Icon name="play" size={45} color={"#fffbea"}></Icon>
+        </View>
+        <View style={styles.text_container}>
+          <Text style={styles.text_font}>회원가입</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default Join;
+
+const styles = StyleSheet.create({
+  button:{
+    flexDirection: 'row',
+    width: "80%",
+    height: "40%",
+    backgroundColor: "#ffea93",
+  },
+  icon_container:{
+    height: '100%',
+    width: '25%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor: '#ff0000',
+  },
+  text_container:{
+    height: '100%',
+    width: '75%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text_font:{
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
