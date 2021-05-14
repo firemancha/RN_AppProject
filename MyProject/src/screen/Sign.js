b0ab4186d1b6332e846a3b34098eb597ba369c76
Cha_Y_S <vaite714@gmail.com>
2021-05-14 21:32:20 +0900

    수정: Sign.js / 로그인 페이지 구성 수정

--- MyProject/src/screen/Sign.js
+++ MyProject/src/screen/Sign.js
@@ -10,6 +10,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import DStyle from '../component/DefaultStyle';
 import FindPassWord from '../component/FindPW';
 import Join from '../component/Join';
+import Login from '../component/Login';
 
 class SignScreen extends Component {
 	render(){
@@ -32,7 +33,32 @@ class SignScreen extends Component {
 							<Text style={styles.title_font}>로그인 페이지</Text>
 						</View>
 						<View style={styles.content}>
-
+							<View style={styles.text_container}>
+								<View style={styles.content_text}>
+									<View style={styles.id_container}>
+										<Text style={styles.content_font}>아이디</Text>
+									</View>
+									<View style={styles.pw_container}>
+										<Text style={styles.content_font}>비밀번호</Text>
+									</View>
+								</View>
+								<View style={styles.content_input}>
+									<View style={styles.id_container}>
+										<TextInput style={styles.content_inputbox}
+											placeholder={"ID"}
+										/>
+									</View>
+									<View style={styles.pw_container}>
+										<TextInput style={styles.content_inputbox}
+											placeholder={"PassWord"}
+											secureTextEntry={true}
+										/>
+									</View>
+								</View>
+							</View>
+							<View style={styles.button_container}>
+									<Login />
+							</View>
 						</View>
 						<View style={styles.footer}>
 							<FindPassWord />
@@ -72,13 +98,67 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold',
 	},
 	content:{
+		// flexDirection: 'row',
 		flex: 1,
 		width: '100%',
-		backgroundColor: '#0000ff',
+		// backgroundColor: '#0000ff',
 		// paddingVertical: 100,
 		justifyContent: 'space-around',
 	},
-
+	text_container:{
+		flexDirection: 'row',
+		height:'80%',
+		width: '100%',
+		// backgroundColor: '#ff0000',
+	},
+	content_text:{
+		// flex:1,
+		height: '100%',
+		width:'30%',
+		// backgroundColor: '#ff0000',
+	},
+	content_input:{
+		// flex:1,
+		height:'100%',
+		width: '70%',
+		// backgroundColor: '#ff00ff',
+	},
+	id_container:{
+		height: '50%',
+		width:'100%',
+		// backgroundColor: '#f0ff0f',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	pw_container:{
+		height: '50%',
+		width: '100%',
+		// backgroundColor: '#00f0ff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	content_font:{
+		fontSize: 25,
+		fontWeight: 'bold',
+	},
+	content_inputbox:{
+		width: '80%',
+		height: 40,
+		borderWidth: 2,
+		borderColor: '#fff4c7',
+		fontSize: 15,
+		// alignItems: 'center',
+		// justifyContent: 'center',
+	},
+	button_container:{
+		// flex: 1,
+		height:'20%',
+		width:'100%',
+		// backgroundColor: '#ffffff',
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		paddingHorizontal: 30,
+	},
 	footer:{
 		width:'100%',
 		height:'30%',
