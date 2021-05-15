8953bfa5944c19c34ebc9b8d41b298bef75478c4
Cha_Y_S <vaite714@gmail.com>
2021-05-16 04:33:11 +0900

    수정: UserInfoStore.js / Alert Customizing

--- MyProject/src/stores/UserInfoStore.js
+++ MyProject/src/stores/UserInfoStore.js
@@ -1,5 +1,5 @@
 import { observable, action, computed } from 'mobx';
-import { Keyboard } from 'react-native';
+import { Keyboard, Alert } from 'react-native';
 
 class UserInfoStore {
   @observable userID = ""
@@ -16,7 +16,20 @@ class UserInfoStore {
   }
 
   checkUserInfoHandler = () => {
-    alert(`UserID: ${this.userID}\nUserPW: ${this.userPW}`);
+    const alertMessage = `아이디: ${this.userID}\n비밀번호: ${this.userPW}`;
+    Alert.alert(
+      // 회원 정보 확인`,
+      // `아이디: ${this.userID}\비밀번호: ${this.userPW}`
+      "회원 정보 확인",
+      `아이디: ${this.userID}\n비밀번호: ${this.userPW}`,
+      [
+        {
+          text: "확인",
+          // onPress: () => console.log('Close Alert'),
+        }
+      ],
+      // { cancelable: false }
+    );
     Keyboard.dismiss();
   }
 }
