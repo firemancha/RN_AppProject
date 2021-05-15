95862c17dcff521d19a5b724aae8e2f3ce821048
Cha_Y_S <vaite714@gmail.com>
2021-05-16 03:59:10 +0900

    수정: Join.js / 회원가입 버튼 borderRadius 수정

--- MyProject/src/component/Join.js
+++ MyProject/src/component/Join.js
@@ -29,6 +29,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width: "80%",
     height: "40%",
+    borderRadius: 8,
     backgroundColor: "#ffea93",
   },
   icon_container:{
