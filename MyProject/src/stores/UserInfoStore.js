6f602a3bc45286dc86040d9b0c569336b83c07a4
Cha_Y_S <vaite714@gmail.com>
2021-05-16 03:57:14 +0900

    추가: UserInfoStore.js / 각 이벤트별 핸들러 추가

--- MyProject/src/stores/UserInfoStore.js
+++ MyProject/src/stores/UserInfoStore.js
@@ -1,9 +1,24 @@
 import { observable, action, computed } from 'mobx';
+import { Keyboard } from 'react-native';
 
 class UserInfoStore {
   @observable userID = ""
   @observable userPW = ""
 
+  @action handleUserID = ID => {
+    this.userID = ID;
+    // console.log("Called handleUserID: ", this.userID);
+  }
+
+  @action handleUserPW = PW => {
+    this.userPW = PW;
+    // console.log("Called handleUserPW: ", this.userPW);
+  }
+
+  checkUserInfoHandler = () => {
+    alert(`UserID: ${this.userID}\nUserPW: ${this.userPW}`);
+    Keyboard.dismiss();
+  }
 }
 
 export default UserInfoStore;
